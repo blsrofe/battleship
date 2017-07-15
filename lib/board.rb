5c7d4c0c57e4094326c741b277882523388bdc10
Becki Srofe <blf_23@yahoo.com>
2017-07-15 14:43:17 -0600

    Add find_square method

--- lib/board.rb
+++ lib/board.rb
@@ -7,8 +7,15 @@ class Board
   def initialize
     @layout = [{"A1" => Square.new}, {"A2" => Square.new}, {"A3" => Square.new}, {"A4" => Square.new},
               {"B1" => Square.new}, {"B2" => Square.new}, {"B3" => Square.new}, {"B4" => Square.new},
-              {"C1" => Square.new}, {"C2" => Square.new}, {"C4" => Square.new}, {"C4" => Square.new},
+              {"C1" => Square.new}, {"C2" => Square.new}, {"C3" => Square.new}, {"C4" => Square.new},
               {"D1" => Square.new}, {"D2" => Square.new}, {"D3" => Square.new}, {"D4" => Square.new}]
+  end
+
+  def find_square(key)
+    new_square = @layout.detect do |square|
+      square.keys.join == key
+    end
+    new_square
   end
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -7,5 +7,11 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, Board.new
   end
 
+  def test_it_can_find_square
+    board = Board.new
+    square = board.layout[10]
+    assert_equal square, board.find_square("C3")
+  end
+
 
 end
