7bc11c786092ab6fd01963bcd20cd337edfdcd56
Becki Srofe <blf_23@yahoo.com>
2017-07-15 08:08:06 -0600

    Add game tests

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,13 @@ require './lib/square'
 
 class Board
 
-  attr_reader :row_1,
-              :row_2,
-              :row_3,
-              :row_4
+  attr_reader :layout
 
   def initialize
-    @row_1 = [{"A1" => Square.new}, {"A2" => Square.new}, {"A3" => Square.new}, {"A4" => Square.new}]
-    @row_2 = [{"B1" => Square.new}, {"B2" => Square.new}, {"B3" => Square.new}, {"B4" => Square.new}]
-    @row_3 = [{"C1" => Square.new}, {"C2" => Square.new}, {"C4" => Square.new}, {"C4" => Square.new}]
-    @row_4 = [{"D1" => Square.new}, {"D2" => Square.new}, {"D3" => Square.new}, {"D4" => Square.new}]
+    @layout = [{"A1" => Square.new}, {"A2" => Square.new}, {"A3" => Square.new}, {"A4" => Square.new},
+              {"B1" => Square.new}, {"B2" => Square.new}, {"B3" => Square.new}, {"B4" => Square.new},
+              {"C1" => Square.new}, {"C2" => Square.new}, {"C4" => Square.new}, {"C4" => Square.new},
+              {"D1" => Square.new}, {"D2" => Square.new}, {"D3" => Square.new}, {"D4" => Square.new}]
   end
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -7,11 +7,29 @@ class GameTest < Minitest::Test
   end
 
   def test_lowercase_or_capital_i_returns_info
-    skip
+    skip#test start_sequence scenarios
     game = Game.new
     instructions = "You have two ships. You will be prompted to place your ships."
     assert_equal instructions, game.start_sequence("i")
+  end
 
+  def test_can_give_instrutions#why does it run start_sequence when I do this test
+    skip
+    game = Game.new
+    instructions = "You have two ships. You will be prompted to place your ships."
+    assert_equal instructions, game.give_instructions
+  end
+
+  def test_computer_can_place_ships
+    skip
+    game = Game.new
+    message = "Your opponent has placed their ships. Now you need to place your ships"
+    assert_equal message, game.computer_ship_placement
+  end
+
+  def test_can_find_random_square_to_start_placing_ships
+    start = game.find_random_start_square
+    assert board.layout.include?(start)#may need to change based off configuration of layout
   end
 
 end
